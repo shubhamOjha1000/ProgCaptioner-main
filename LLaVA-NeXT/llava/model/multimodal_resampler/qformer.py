c0d1bda6ca0a80ba601680142c324c2a2cd19cc1
Shubham Ojha <shubham.ojha1000@gmail.com>
2026-03-31 06:20:27 -0400

    c

--- LLaVA-NeXT/llava/model/multimodal_resampler/qformer.py
+++ LLaVA-NeXT/llava/model/multimodal_resampler/qformer.py
@@ -36,11 +36,11 @@ from transformers.modeling_outputs import (
     SequenceClassifierOutput,
     TokenClassifierOutput,
 )
-from transformers.modeling_utils import (
-    PreTrainedModel,
-    find_pruneable_heads_and_indices,
-    prune_linear_layer,
-)
+from transformers.modeling_utils import PreTrainedModel
+try:
+    from transformers.pytorch_utils import find_pruneable_heads_and_indices, prune_linear_layer
+except ImportError:
+    from transformers.modeling_utils import find_pruneable_heads_and_indices, prune_linear_layer
 try:
     from transformers.pytorch_utils import apply_chunking_to_forward
 except ImportError:
